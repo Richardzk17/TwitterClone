2c07778787c01c8bf2d5c047f348b6253f50d4b2
richardzk17 <richardhernandezk17@gmail.com>
2024-01-21 11:18:59 -0500

    add comment

--- controllers/tweets.js
+++ controllers/tweets.js
@@ -14,6 +14,27 @@ function index(req, res) {
   })
 }
 
+function addComment(req, res) {
+  Tweet.findById(req.params.tweetId)
+  .then(tweet => {
+    req.body.author = req.user.profile._id
+    tweet.content.push(req.body)
+    tweet.save()
+    .then(()=> {
+      res.redirect(`/tweets/${tweet._id}`)
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/tweets')
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/tweets')
+  })
+}
+
 export {
-  index
+  index,
+  addComment,
 }
--- routes/tweets.js
+++ routes/tweets.js
@@ -1,9 +1,14 @@
 import { Router } from 'express'
 import * as tweetsCtrl from '../controllers/tweets.js'
+import { isLoggedIn } from "../middleware/middleware.js";
 
 const router = Router()
 
+
+// GET localhost:3000/tweets
 router.get('/', tweetsCtrl.index)
+
+router.post('/:tweetId/content', isLoggedIn, tweetsCtrl.addComment)
 
 export {
   router
